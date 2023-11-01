6faafca6dfa2bfdd36467d6ba2f2907939cfc069
farahnini <farahnini98@gmail.com>
2023-11-01 16:56:47 +0800

    Create migration

---
+++ database/migrations/2023_11_01_084608_create_inventory_item_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_item_settings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->reference('item_inventories');
+            $table->integer('minimum_number_item_alert');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_item_settings');
+    }
+};
