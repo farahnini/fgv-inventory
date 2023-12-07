75836a099304d102f279de7fc09119b9d82bf6ae
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2023-12-07 15:42:04 +0800

    add authentication log

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use OwenIt\Auditing\Contracts\Auditable;
+use Yadahan\AuthenticationLog\AuthenticationLogable;
 
 class User extends Authenticatable implements Auditable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use \OwenIt\Auditing\Auditable;
+    use AuthenticationLogable;
 
     /**
      * The attributes that are mass assignable.
