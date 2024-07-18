fe210cdd18c280fb1117546132d3bbdcab36a071
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-07-18 10:10:48 +0800

    add policy

---
+++ app/Policies/InventoryCategoryPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\InventoryCategory;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class InventoryCategoryPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, InventoryCategory $inventoryCategory): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, InventoryCategory $inventoryCategory): bool
+    {
+        // check if $inventoryCategory created less than 5 minute from current time
+        return $inventoryCategory->created_at->diffInMinutes(now()) < 5 ? true : false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, InventoryCategory $inventoryCategory): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, InventoryCategory $inventoryCategory): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, InventoryCategory $inventoryCategory): bool
+    {
+        //
+    }
+}
