584fac1fa66ac7b43978bfbe29203cebe5f3764b
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-08-21 10:51:07 +0800

    add permission checker on user index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -56,7 +56,9 @@
                 <div class="card-header">
                     <div class="d-flex justify-content-between align-items-center">
                         <span>{{ __('Users Index') }}</span>
-                        <a href="{{ route('users.create') }}" class="btn btn-primary">+ User</a>
+                        @can('user-create')
+                            <a href="{{ route('users.create') }}" class="btn btn-primary">+ User</a>
+                        @endcan
                     </div>
                 </div>
 
@@ -89,12 +91,16 @@
                                             <a href="{{ route('users.show', $user) }}" class="btn btn-sm btn-primary" style="margin-right:2px;">
                                                 <i class="fas fa-eye"></i>
                                             </a>
+                                            @can('user-edit')
                                             <a href="{{ route('users.edit', $user) }}" class="btn btn-sm btn-success" style="margin-right:2px;">
                                                 <i class="fas fa-edit"></i> 
                                             </a>
+                                            @endcan
+                                            @can('user-delete')
                                             <a href="{{ route('users.delete', $user) }}" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this user?');">
                                                 <i class="fas fa-trash-alt"></i>
                                             </a>
+                                            @endcan
                                         </td>
                                     </tr>
                                 @endforeach
